6704d07a9ca99a7b4a2641c172f881186c49d904
bendn <bend.n@outlook.com>
2022-07-07 07:38:51 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# gdcereal
+
+### build steps:
+
+```
+scons
+```
+
+Inspired by:
+https://github.com/valentegamedev/arduino_godot
---
+++ serial.gd
@@ -0,0 +1,27 @@
+extends Reference
+
+const Serial = preload("res://bin/gdserial.gdns")
+var serial: Serial
+
+signal recieved()
+
+const baud_rate := 9600
+const endline := "\n"
+
+
+#@param text the text to send
+func write(text: String) -> void:
+	serial.send(text)
+
+
+func create_serial():
+	if serial:
+		serial.end()
+	serial = Serial.new()
+	serial.start("/dev/ttyACM0", baud_rate)
+
+
+func _process(delta):
+	if serial.get_available() > 0:
+		var data = serial.read_string()
+		emit_signal("recieved", data)
